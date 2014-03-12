d601740d39b1ef4ee848fa7a912c23503a3401b8
cheshireoctopus <chandlermoisen@gmail.com>
2014-03-12 13:26:50 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ClListingsApp::Application.config.secret_key_base = 'b38c9282c4e1f940e46f91276bca3a8baf9511c068c7f547a8705b85552496c20df7cc2165f25a098ea0969b1da455b48059d30093cdc708ca9ed4d95436271d'
